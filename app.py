a85fb2d897ccd9a43bf1c0fc0ce581550ed576b8
Lem Ticman <lemticman@Lems-MacBook-Pro.local>
2025-05-19 13:27:17 +0800

    Fix: serve index.html from static folder

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ inventory_data = []
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return app.send_static_file('index.html')
 
 @app.route('/api/inventory', methods=['GET'])
 def get_inventory():
