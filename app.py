45b441966a2b782749eaa1caa0cc16ff513de2e6
Lem Ticman <lemticman@Lems-MacBook-Pro.local>
2025-05-19 11:32:51 +0800

    Fix: use Railway-assigned port only

--- app.py
+++ app.py
@@ -23,6 +23,6 @@ def add_inventory():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT"))
+    port = int(os.environ.get("PORT"])
     app.run(host="0.0.0.0", port=port)
 
